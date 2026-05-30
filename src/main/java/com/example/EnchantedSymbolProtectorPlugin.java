381d9b5a4a0e1e237cafa4fe10b0830a85e7da35
Zachary Jacobson <zach.h.jacobson@gmail.com>
2026-05-29 23:56:47 -0500

    Replace template filenames

--- src/main/java/com/example/EnchantedSymbolProtectorPlugin.java
+++ src/main/java/com/example/EnchantedSymbolProtectorPlugin.java
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class EnchantedSymbolProtectorPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private EnchantedSymbolProtectorConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	EnchantedSymbolProtectorConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(EnchantedSymbolProtectorConfig.class);
 	}
 }
